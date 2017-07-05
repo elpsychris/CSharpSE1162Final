21d2a39fd13f44d7a6231972643e0ee80038e02b
Tung Nguyên <rohirrim3105@gmail.com>
2017-07-05 05:34:32 -0700

    DB Schema v0.2: added new table, commented some spare columns,... please check in the internal README comment

--- DBScript.sql
+++ DBScript.sql
@@ -1,4 +1,13 @@
-﻿--CREATE DATABASE OnlineLibrary
+﻿--README
+--v0.1: Remove SEQ of tblRole
+--		Decomposed tblBook into tblTitle and TblBook
+--		Change relationship of tblTitle and tblCategory into M-M (Many-Many)
+--
+--v0.2:	Add tblBookStatus ref to tblBook 
+--		Add col 'ISBN' in tblTitle
+--		Add tblFineType ref to tblFineActivity
+
+--CREATE DATABASE OnlineLibrary
 --USE OnlineLibrary
 
 
@@ -16,7 +25,8 @@ CREATE TABLE Title
 	Name nvarchar(100),
 	Publisher nvarchar(50),
 	PublishDate date,
-	Quantity int
+	ISBN char(13), --add in v0.1
+	AvailableQuan int --Available quantity
 )
 
 
@@ -30,12 +40,18 @@ CREATE TABLE Category
 
 
 --Phân loại ngày trả sách tùy theo loại sách
-CREATE TABLE ReturnType
+CREATE TABLE Return_Type
 (
 	SEQ int identity(1, 1) primary key,
 	NoOfDay int
 )
 
+--Thông tin về tình trạng sách (Đã cho mượn, Đang trong kho, Đã chuyển đi, Đã mất Ref: library.uncw.edu/faq/what_does_status_library_catalog_mean
+CREATE TABLE Book_Status
+(
+	StatusID int primary key,
+	StatusName varchar(50)
+)
 
 --Thông tin về sách
 CREATE TABLE Book
@@ -44,9 +60,9 @@ CREATE TABLE Book
 	ID varchar(10) unique,
 	Title int CONSTRAINT FK_B_T REFERENCES Title(SEQ),
 	Author nvarchar(50),
-	ReturnType int CONSTRAINT FK_B_RT REFERENCES ReturnType(SEQ),
+	ReturnType int CONSTRAINT FK_B_RT REFERENCES Return_Type(SEQ),
 	ImportDate date NOT NULL,
-	Borrowed bit
+	StatusID int CONSTRAINT FK_B_BST REFERENCES Book_Status(StatusID)
 )
 
 --Thông tin category tương ứng book
@@ -82,7 +98,14 @@ CREATE TABLE Activity_History
 	BID varchar(10) CONSTRAINT FK_AH_B REFERENCES Book(ID),
 	BorrowDate date,--ngày mượn sách
 	ReturnDate date,--ngày trả sách (sẽ update khi member đến trả)
+)
 
+
+--Ghi lại các loại lỗi có thể bị phạt
+CREATE TABLE Fine_Type
+(
+	FineTypeID int primary key,
+	FineTypeName varchar(50)
 )
 
 
@@ -90,12 +113,16 @@ CREATE TABLE Activity_History
 --Kết hợp với bảng hoạt động ở trên, VD: member nào có mặt trong bảng
 --trên và cả ở bảng này nghĩa là member đó trả trễ sách (tìm dựa trên
 --activity ID)
-CREATE TABLE Late_Returner
+CREATE TABLE Fine_History
 (
 	SEQ int identity(1, 1) primary key,
 	ASEQ int unique CONSTRAINT FK_LR_AH REFERENCES Activity_History(SEQ),
+	FineTypeID int CONSTRAINT FK_FH_FT REFERENCES Fine_Type(FineTypeID), --add in v0.1 
 	FineAmount float,--số tiền phạt
-	DaysLate int,--số ngày trễ
+	--DaysLate int,--số ngày trễ 
+	--(Không cần thiết do bảng này đã tổng
+	--quát hóa cho nhiều loại phí phạt: mất sách, rách sách,
+	--trả trễ)
 )
 
 --Ghi lại danh sách yêu thích của member
